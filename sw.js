24132a0feee797db026b402fd04986fbfe87b850
7-7-7-8 alt account <logan.henderson@murtaugh.k12.id.us>
2026-07-08 15:36:44 -0600

    Implement image caching in service worker
    Add fetch event listener to cache images

--- sw.js
+++ sw.js
@@ -71,3 +71,44 @@ event.request
 );
 
 });
+self.addEventListener(
+"fetch",
+event=>{
+
+if(
+event.request.destination
+=== "image"
+){
+
+event.respondWith(
+
+caches.open("images")
+.then(async cache=>{
+
+const cached =
+await cache.match(
+event.request
+);
+
+if(cached)
+    return cached;
+
+const fresh =
+await fetch(
+event.request
+);
+
+cache.put(
+event.request,
+fresh.clone()
+);
+
+return fresh;
+
+})
+
+);
+
+}
+
+});
